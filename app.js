2906ebda6ea446d468da6f7eabd7fe107f7266c1
Moharram Moharrami <moharram.m98@gmail.com>
2022-01-20 19:16:03 +0330

    avoid ghost lair and implement shortcut

--- app.js
+++ app.js
@@ -54,6 +54,8 @@ function createBoard() {
             squares[i].classList.add('pac-dot');
         } else if (layout[i] === 1) {
             squares[i].classList.add('wall');
+        } else if (layout[i] === 2) {
+            squares[i].classList.add('ghost-lair');
         } else if (layout[i] === 3) {
             squares[i].classList.add('power-pellet');
         }
@@ -72,25 +74,35 @@ function control(e) {
     switch(e.keyCode) {
         case 37:
             console.log('pressed left');
-            if (!squares[pacmanCurrentIndex - 1].classList.contains('wall') &&
+            if (!squares[pacmanCurrentIndex - 1].classList.contains('ghost-lair') &&
+                !squares[pacmanCurrentIndex - 1].classList.contains('wall') &&
                 pacmanCurrentIndex % width !== 0)
                     pacmanCurrentIndex -= 1;
+            if (pacmanCurrentIndex === 364) {
+                pacmanCurrentIndex = 391;
+            }
             break;
         case 38:
             console.log('pressed up');
-            if (!squares[pacmanCurrentIndex - width].classList.contains('wall') &&
+            if (!squares[pacmanCurrentIndex - width].classList.contains('ghost-lair') &&
+                !squares[pacmanCurrentIndex - width].classList.contains('wall') &&
                 pacmanCurrentIndex - width >= 0)
                     pacmanCurrentIndex -= width;
             break;
         case 39:
             console.log('pressed right');
-            if (!squares[pacmanCurrentIndex + 1].classList.contains('wall') &&
+            if (!squares[pacmanCurrentIndex + 1].classList.contains('ghost-lair') &&
+                !squares[pacmanCurrentIndex + 1].classList.contains('wall') &&
                 pacmanCurrentIndex % width < width - 1)
                     pacmanCurrentIndex += 1;
+            if (pacmanCurrentIndex === 391) {
+                pacmanCurrentIndex = 364;
+            }
             break;
         case 40:
             console.log('pressed down');
-            if (!squares[pacmanCurrentIndex + width].classList.contains('wall') &&
+            if (!squares[pacmanCurrentIndex + width].classList.contains('ghost-lair') &&
+                !squares[pacmanCurrentIndex + width].classList.contains('wall') &&
                 pacmanCurrentIndex + width < width * width) 
                     pacmanCurrentIndex += width;
             break;
