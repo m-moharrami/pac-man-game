0c89ebf6dd11b9bd5541d76e1046f28414094435
Moharram Moharrami <moharram.m98@gmail.com>
2022-01-21 16:10:31 +0330

    make ghosts move while avoiding walls and each other

--- app.js
+++ app.js
@@ -141,4 +141,30 @@ const ghosts = [
     new Ghost('clyde', 379, 500)
 ];
 
-ghosts.forEach(ghost => squares[ghost.startIndex].classList.add(ghost.className));
+// draw ghosts onto board
+ghosts.forEach(ghost => {
+    squares[ghost.currentIndex].classList.add(ghost.className);
+    squares[ghost.currentIndex].classList.add('ghost');
+});
+// move the ghosts
+ghosts.forEach(ghost => moveGhost(ghost));
+
+function moveGhost(ghost) {
+    console.log('moved ghost');
+    const directions = [-1, +1, -width, +width];
+    let direction = directions[Math.floor(Math.random() * directions.length)];
+    console.log(direction); 
+
+    ghost.timerId = setInterval(function() {
+        if (!squares[ghost.currentIndex + direction].classList.contains('wall') &&
+            !squares[ghost.currentIndex + direction].classList.contains('ghost')) {
+                squares[ghost.currentIndex].classList.remove(ghost.className);
+                squares[ghost.currentIndex].classList.remove('ghost');
+                ghost.currentIndex += direction;
+                squares[ghost.currentIndex].classList.add(ghost.className);
+                squares[ghost.currentIndex].classList.add('ghost');
+            } else {
+                direction = directions[Math.floor(Math.random() * directions.length)];
+            }
+    }, ghost.speed);
+}
